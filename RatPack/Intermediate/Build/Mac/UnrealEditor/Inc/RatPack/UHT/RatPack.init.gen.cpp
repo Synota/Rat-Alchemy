8259a0cc56c1df7ec127625c51536054fe2175e9
diegorod <diegorod@mit.edu>
2023-11-14 14:26:04 -0500

    added initial Unreal template

---
+++ RatPack/Intermediate/Build/Mac/UnrealEditor/Inc/RatPack/UHT/RatPack.init.gen.cpp
@@ -0,0 +1,29 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeRatPack_init() {}
+	static FPackageRegistrationInfo Z_Registration_Info_UPackage__Script_RatPack;
+	FORCENOINLINE UPackage* Z_Construct_UPackage__Script_RatPack()
+	{
+		if (!Z_Registration_Info_UPackage__Script_RatPack.OuterSingleton)
+		{
+			static const UECodeGen_Private::FPackageParams PackageParams = {
+				"/Script/RatPack",
+				nullptr,
+				0,
+				PKG_CompiledIn | 0x00000000,
+				0xAAD0E7E9,
+				0xFDEA2510,
+				METADATA_PARAMS(0, nullptr)
+			};
+			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_RatPack.OuterSingleton, PackageParams);
+		}
+		return Z_Registration_Info_UPackage__Script_RatPack.OuterSingleton;
+	}
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_RatPack(Z_Construct_UPackage__Script_RatPack, TEXT("/Script/RatPack"), Z_Registration_Info_UPackage__Script_RatPack, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xAAD0E7E9, 0xFDEA2510));
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ RatPack/Intermediate/Build/Mac/UnrealEditor/Inc/RatPack/UHT/RatPackPlayerController.generated.h
@@ -0,0 +1,64 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "RatPackPlayerController.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef RATPACK_RatPackPlayerController_generated_h
+#error "RatPackPlayerController.generated.h already included, missing '#pragma once' in RatPackPlayerController.h"
+#endif
+#define RATPACK_RatPackPlayerController_generated_h
+
+#define FID_Diego1_Documents_Unreal_Projects_RatPack_Source_RatPack_RatPackPlayerController_h_20_SPARSE_DATA
+#define FID_Diego1_Documents_Unreal_Projects_RatPack_Source_RatPack_RatPackPlayerController_h_20_SPARSE_DATA_PROPERTY_ACCESSORS
+#define FID_Diego1_Documents_Unreal_Projects_RatPack_Source_RatPack_RatPackPlayerController_h_20_EDITOR_ONLY_SPARSE_DATA_PROPERTY_ACCESSORS
+#define FID_Diego1_Documents_Unreal_Projects_RatPack_Source_RatPack_RatPackPlayerController_h_20_RPC_WRAPPERS_NO_PURE_DECLS
+#define FID_Diego1_Documents_Unreal_Projects_RatPack_Source_RatPack_RatPackPlayerController_h_20_ACCESSORS
+#define FID_Diego1_Documents_Unreal_Projects_RatPack_Source_RatPack_RatPackPlayerController_h_20_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesARatPackPlayerController(); \
+	friend struct Z_Construct_UClass_ARatPackPlayerController_Statics; \
+public: \
+	DECLARE_CLASS(ARatPackPlayerController, APlayerController, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/RatPack"), NO_API) \
+	DECLARE_SERIALIZER(ARatPackPlayerController)
+
+
+#define FID_Diego1_Documents_Unreal_Projects_RatPack_Source_RatPack_RatPackPlayerController_h_20_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ARatPackPlayerController(ARatPackPlayerController&&); \
+	NO_API ARatPackPlayerController(const ARatPackPlayerController&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ARatPackPlayerController); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ARatPackPlayerController); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(ARatPackPlayerController) \
+	NO_API virtual ~ARatPackPlayerController();
+
+
+#define FID_Diego1_Documents_Unreal_Projects_RatPack_Source_RatPack_RatPackPlayerController_h_17_PROLOG
+#define FID_Diego1_Documents_Unreal_Projects_RatPack_Source_RatPack_RatPackPlayerController_h_20_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_Diego1_Documents_Unreal_Projects_RatPack_Source_RatPack_RatPackPlayerController_h_20_SPARSE_DATA \
+	FID_Diego1_Documents_Unreal_Projects_RatPack_Source_RatPack_RatPackPlayerController_h_20_SPARSE_DATA_PROPERTY_ACCESSORS \
+	FID_Diego1_Documents_Unreal_Projects_RatPack_Source_RatPack_RatPackPlayerController_h_20_EDITOR_ONLY_SPARSE_DATA_PROPERTY_ACCESSORS \
+	FID_Diego1_Documents_Unreal_Projects_RatPack_Source_RatPack_RatPackPlayerController_h_20_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_Diego1_Documents_Unreal_Projects_RatPack_Source_RatPack_RatPackPlayerController_h_20_ACCESSORS \
+	FID_Diego1_Documents_Unreal_Projects_RatPack_Source_RatPack_RatPackPlayerController_h_20_INCLASS_NO_PURE_DECLS \
+	FID_Diego1_Documents_Unreal_Projects_RatPack_Source_RatPack_RatPackPlayerController_h_20_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> RATPACK_API UClass* StaticClass<class ARatPackPlayerController>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_Diego1_Documents_Unreal_Projects_RatPack_Source_RatPack_RatPackPlayerController_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
