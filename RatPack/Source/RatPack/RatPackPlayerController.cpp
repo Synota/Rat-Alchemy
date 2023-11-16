10751db26586f81548fc200cf3c542cebb40e062
diegorod <diegorod@mit.edu>
2023-11-16 15:08:49 -0500

    removed original cpp code

--- RatPack/Source/RatPack/RatPackPlayerController.cpp
+++ RatPack/Source/RatPack/RatPackPlayerController.cpp
@@ -1,125 +1,125 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "RatPackPlayerController.h"
-#include "GameFramework/Pawn.h"
-#include "Blueprint/AIBlueprintHelperLibrary.h"
-#include "NiagaraSystem.h"
-#include "NiagaraFunctionLibrary.h"
-#include "RatPackCharacter.h"
-#include "Engine/World.h"
-#include "EnhancedInputComponent.h"
-#include "InputActionValue.h"
-#include "EnhancedInputSubsystems.h"
-#include "Engine/LocalPlayer.h"
-
-DEFINE_LOG_CATEGORY(LogTemplateCharacter);
-
-ARatPackPlayerController::ARatPackPlayerController()
-{
-	bShowMouseCursor = true;
-	DefaultMouseCursor = EMouseCursor::Default;
-	CachedDestination = FVector::ZeroVector;
-	FollowTime = 0.f;
-}
-
-void ARatPackPlayerController::BeginPlay()
-{
-	// Call the base class  
-	Super::BeginPlay();
-
-	//Add Input Mapping Context
-	if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer()))
-	{
-		Subsystem->AddMappingContext(DefaultMappingContext, 0);
-	}
-}
-
-void ARatPackPlayerController::SetupInputComponent()
-{
-	// set up gameplay key bindings
-	Super::SetupInputComponent();
-
-	// Set up action bindings
-	if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(InputComponent))
-	{
-		// Setup mouse input events
-		EnhancedInputComponent->BindAction(SetDestinationClickAction, ETriggerEvent::Started, this, &ARatPackPlayerController::OnInputStarted);
-		EnhancedInputComponent->BindAction(SetDestinationClickAction, ETriggerEvent::Triggered, this, &ARatPackPlayerController::OnSetDestinationTriggered);
-		EnhancedInputComponent->BindAction(SetDestinationClickAction, ETriggerEvent::Completed, this, &ARatPackPlayerController::OnSetDestinationReleased);
-		EnhancedInputComponent->BindAction(SetDestinationClickAction, ETriggerEvent::Canceled, this, &ARatPackPlayerController::OnSetDestinationReleased);
-
-		// Setup touch input events
-		EnhancedInputComponent->BindAction(SetDestinationTouchAction, ETriggerEvent::Started, this, &ARatPackPlayerController::OnInputStarted);
-		EnhancedInputComponent->BindAction(SetDestinationTouchAction, ETriggerEvent::Triggered, this, &ARatPackPlayerController::OnTouchTriggered);
-		EnhancedInputComponent->BindAction(SetDestinationTouchAction, ETriggerEvent::Completed, this, &ARatPackPlayerController::OnTouchReleased);
-		EnhancedInputComponent->BindAction(SetDestinationTouchAction, ETriggerEvent::Canceled, this, &ARatPackPlayerController::OnTouchReleased);
-	}
-	else
-	{
-		UE_LOG(LogTemplateCharacter, Error, TEXT("'%s' Failed to find an Enhanced Input Component! This template is built to use the Enhanced Input system. If you intend to use the legacy system, then you will need to update this C++ file."), *GetNameSafe(this));
-	}
-}
-
-void ARatPackPlayerController::OnInputStarted()
-{
-	StopMovement();
-}
-
-// Triggered every frame when the input is held down
-void ARatPackPlayerController::OnSetDestinationTriggered()
-{
-	// We flag that the input is being pressed
-	FollowTime += GetWorld()->GetDeltaSeconds();
-	
-	// We look for the location in the world where the player has pressed the input
-	FHitResult Hit;
-	bool bHitSuccessful = false;
-	if (bIsTouch)
-	{
-		bHitSuccessful = GetHitResultUnderFinger(ETouchIndex::Touch1, ECollisionChannel::ECC_Visibility, true, Hit);
-	}
-	else
-	{
-		bHitSuccessful = GetHitResultUnderCursor(ECollisionChannel::ECC_Visibility, true, Hit);
-	}
-
-	// If we hit a surface, cache the location
-	if (bHitSuccessful)
-	{
-		CachedDestination = Hit.Location;
-	}
-	
-	// Move towards mouse pointer or touch
-	APawn* ControlledPawn = GetPawn();
-	if (ControlledPawn != nullptr)
-	{
-		FVector WorldDirection = (CachedDestination - ControlledPawn->GetActorLocation()).GetSafeNormal();
-		ControlledPawn->AddMovementInput(WorldDirection, 1.0, false);
-	}
-}
-
-void ARatPackPlayerController::OnSetDestinationReleased()
-{
-	// If it was a short press
-	if (FollowTime <= ShortPressThreshold)
-	{
-		// We move there and spawn some particles
-		UAIBlueprintHelperLibrary::SimpleMoveToLocation(this, CachedDestination);
-		UNiagaraFunctionLibrary::SpawnSystemAtLocation(this, FXCursor, CachedDestination, FRotator::ZeroRotator, FVector(1.f, 1.f, 1.f), true, true, ENCPoolMethod::None, true);
-	}
-
-	FollowTime = 0.f;
-}
-
-// Triggered every frame when the input is held down
-void ARatPackPlayerController::OnTouchTriggered()
-{
-	bIsTouch = true;
-	OnSetDestinationTriggered();
-}
-
-void ARatPackPlayerController::OnTouchReleased()
-{
-	bIsTouch = false;
-	OnSetDestinationReleased();
-}
+//
+//#include "RatPackPlayerController.h"
+//#include "GameFramework/Pawn.h"
+//#include "Blueprint/AIBlueprintHelperLibrary.h"
+//#include "NiagaraSystem.h"
+//#include "NiagaraFunctionLibrary.h"
+//#include "RatPackCharacter.h"
+//#include "Engine/World.h"
+//#include "EnhancedInputComponent.h"
+//#include "InputActionValue.h"
+//#include "EnhancedInputSubsystems.h"
+//#include "Engine/LocalPlayer.h"
+//
+//DEFINE_LOG_CATEGORY(LogTemplateCharacter);
+//
+//ARatPackPlayerController::ARatPackPlayerController()
+//{
+//	bShowMouseCursor = true;
+//	DefaultMouseCursor = EMouseCursor::Default;
+//	CachedDestination = FVector::ZeroVector;
+//	FollowTime = 0.f;
+//}
+//
+//void ARatPackPlayerController::BeginPlay()
+//{
+//	// Call the base class  
+//	Super::BeginPlay();
+//
+//	//Add Input Mapping Context
+//	if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer()))
+//	{
+//		Subsystem->AddMappingContext(DefaultMappingContext, 0);
+//	}
+//}
+//
+//void ARatPackPlayerController::SetupInputComponent()
+//{
+//	// set up gameplay key bindings
+//	Super::SetupInputComponent();
+//
+//	// Set up action bindings
+//	if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(InputComponent))
+//	{
+//		// Setup mouse input events
+//		EnhancedInputComponent->BindAction(SetDestinationClickAction, ETriggerEvent::Started, this, &ARatPackPlayerController::OnInputStarted);
+//		EnhancedInputComponent->BindAction(SetDestinationClickAction, ETriggerEvent::Triggered, this, &ARatPackPlayerController::OnSetDestinationTriggered);
+//		EnhancedInputComponent->BindAction(SetDestinationClickAction, ETriggerEvent::Completed, this, &ARatPackPlayerController::OnSetDestinationReleased);
+//		EnhancedInputComponent->BindAction(SetDestinationClickAction, ETriggerEvent::Canceled, this, &ARatPackPlayerController::OnSetDestinationReleased);
+//
+//		// Setup touch input events
+//		EnhancedInputComponent->BindAction(SetDestinationTouchAction, ETriggerEvent::Started, this, &ARatPackPlayerController::OnInputStarted);
+//		EnhancedInputComponent->BindAction(SetDestinationTouchAction, ETriggerEvent::Triggered, this, &ARatPackPlayerController::OnTouchTriggered);
+//		EnhancedInputComponent->BindAction(SetDestinationTouchAction, ETriggerEvent::Completed, this, &ARatPackPlayerController::OnTouchReleased);
+//		EnhancedInputComponent->BindAction(SetDestinationTouchAction, ETriggerEvent::Canceled, this, &ARatPackPlayerController::OnTouchReleased);
+//	}
+//	else
+//	{
+//		UE_LOG(LogTemplateCharacter, Error, TEXT("'%s' Failed to find an Enhanced Input Component! This template is built to use the Enhanced Input system. If you intend to use the legacy system, then you will need to update this C++ file."), *GetNameSafe(this));
+//	}
+//}
+//
+//void ARatPackPlayerController::OnInputStarted()
+//{
+//	StopMovement();
+//}
+//
+//// Triggered every frame when the input is held down
+//void ARatPackPlayerController::OnSetDestinationTriggered()
+//{
+//	// We flag that the input is being pressed
+//	FollowTime += GetWorld()->GetDeltaSeconds();
+//	
+//	// We look for the location in the world where the player has pressed the input
+//	FHitResult Hit;
+//	bool bHitSuccessful = false;
+//	if (bIsTouch)
+//	{
+//		bHitSuccessful = GetHitResultUnderFinger(ETouchIndex::Touch1, ECollisionChannel::ECC_Visibility, true, Hit);
+//	}
+//	else
+//	{
+//		bHitSuccessful = GetHitResultUnderCursor(ECollisionChannel::ECC_Visibility, true, Hit);
+//	}
+//
+//	// If we hit a surface, cache the location
+//	if (bHitSuccessful)
+//	{
+//		CachedDestination = Hit.Location;
+//	}
+//	
+//	// Move towards mouse pointer or touch
+//	APawn* ControlledPawn = GetPawn();
+//	if (ControlledPawn != nullptr)
+//	{
+//		FVector WorldDirection = (CachedDestination - ControlledPawn->GetActorLocation()).GetSafeNormal();
+//		ControlledPawn->AddMovementInput(WorldDirection, 1.0, false);
+//	}
+//}
+//
+//void ARatPackPlayerController::OnSetDestinationReleased()
+//{
+//	// If it was a short press
+//	if (FollowTime <= ShortPressThreshold)
+//	{
+//		// We move there and spawn some particles
+//		UAIBlueprintHelperLibrary::SimpleMoveToLocation(this, CachedDestination);
+//		UNiagaraFunctionLibrary::SpawnSystemAtLocation(this, FXCursor, CachedDestination, FRotator::ZeroRotator, FVector(1.f, 1.f, 1.f), true, true, ENCPoolMethod::None, true);
+//	}
+//
+//	FollowTime = 0.f;
+//}
+//
+//// Triggered every frame when the input is held down
+//void ARatPackPlayerController::OnTouchTriggered()
+//{
+//	bIsTouch = true;
+//	OnSetDestinationTriggered();
+//}
+//
+//void ARatPackPlayerController::OnTouchReleased()
+//{
+//	bIsTouch = false;
+//	OnSetDestinationReleased();
+//}
