d46334820f79e9f6d45befa57f8760eeb1dd007e
diegorod <diegorod@mit.edu>
2023-11-16 16:13:10 -0500

    Revert "removed original cpp code"
    This reverts commit 10751db26586f81548fc200cf3c542cebb40e062.

--- RatPack/Source/RatPack/RatPackGameMode.h
+++ RatPack/Source/RatPack/RatPackGameMode.h
@@ -1,19 +1,19 @@
-//// Copyright Epic Games, Inc. All Rights Reserved.
-//
-//#pragma once
-//
-//#include "CoreMinimal.h"
-//#include "GameFramework/GameModeBase.h"
-//#include "RatPackGameMode.generated.h"
-//
-//UCLASS(minimalapi)
-//class ARatPackGameMode : public AGameModeBase
-//{
-//	GENERATED_BODY()
-//
-//public:
-//	ARatPackGameMode();
-//};
-//
-//
-//
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RatPackGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ARatPackGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ARatPackGameMode();
+};
+
+
+
